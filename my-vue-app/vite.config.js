6a05f536bc50d3ff84bf942bd852664ada2f8b1b
Berden,Cédric C.P.J <c.berden@student.fontys.nl>
2025-02-26 11:23:59 +0100

    updated sytem for ci/cd

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import nodePolyfills from 'node-polyfill-webpack-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      // Other aliases can go here
+      crypto: require.resolve('crypto-browserify')
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        nodePolyfills()
+      ],
+    },
+  },
+});
+
